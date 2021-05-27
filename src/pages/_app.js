961b37cdbbee5960a7d3c7da52e15d4ee59c60ac
Topleftstyll <joshua.brackett@hotmail.ca>
2021-05-27 00:02:31 -0400

    main page done w responsiveness

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -3,11 +3,11 @@ import { store } from '../app/store'
 import '../styles/globals.css'
 
 const MyApp = ({ Component, pageProps }) => {
-  return (
-    <Provider store={store}>
-      <Component {...pageProps} />
-    </Provider>
-  )
+	return (
+		<Provider store={store}>
+		<Component {...pageProps} />
+		</Provider>
+	)
 }
 
 export default MyApp
